58c0390262e54ac8a5c17131c09d64fd64603503
akash-tyagi <akash.t@tamu.edu>
2014-11-05 19:38:48 -0800

    Adding Files for complete compilation

---
+++ TraceRoute.java
@@ -0,0 +1,268 @@
+package net.floodlightcontroller.traceroute;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
+
+import org.openflow.protocol.OFFlowMod;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.OFMessage;
+import org.openflow.protocol.OFPacketIn;
+import org.openflow.protocol.OFPacketOut;
+import org.openflow.protocol.OFPort;
+import org.openflow.protocol.OFType;
+import org.openflow.protocol.Wildcards;
+import org.openflow.protocol.Wildcards.Flag;
+import org.openflow.protocol.action.OFAction;
+import org.openflow.protocol.action.OFActionOutput;
+import org.openflow.util.U16;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TraceRoute implements IOFMessageListener, IFloodlightModule {
+
+	protected IFloodlightProviderService floodlightProvider;
+	protected static Logger logger;
+	protected static short FLOWMOD_DEFAULT_IDLE_TIMEOUT = 60; // in seconds
+	protected static short FLOWMOD_DEFAULT_HARD_TIMEOUT = 0; // infinite
+	private static int count = 0;
+
+	class HostInfo {
+		int sourceIp;
+		short port;
+	}
+
+	protected Map<Integer, Long> ipToSwitcId;
+	protected Map<Long, Map<Integer, HostInfo>> switchToHostInfo;
+
+	@Override
+	public String getName() {
+		return TraceRoute.class.getSimpleName();
+	}
+
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		return l;
+	}
+
+	@Override
+	public void init(FloodlightModuleContext context)
+			throws FloodlightModuleException {
+		floodlightProvider = context
+				.getServiceImpl(IFloodlightProviderService.class);
+		ipToSwitcId = new HashMap<>();
+		switchToHostInfo = new HashMap<>();
+		logger = LoggerFactory.getLogger(TraceRoute.class);
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context)
+			throws FloodlightModuleException {
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+	}
+
+	@Override
+	public net.floodlightcontroller.core.IListener.Command receive(
+			IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		logger.info("--------------------------------------------------------");
+		switch (msg.getType()) {
+		case PACKET_IN:
+			logger.info("PacketIn Path...");
+			return processPacketInMessage(sw, (OFPacketIn) msg);
+		default:
+			break;
+		}
+
+		return Command.CONTINUE;
+	}
+
+	private net.floodlightcontroller.core.IListener.Command processPacketInMessage(
+			IOFSwitch sw, OFPacketIn msg) {
+		System.out.println("-----------------------------Count:" + count++);
+		OFPacketIn pi = (OFPacketIn) msg;
+		// parse the data in packetIn using match
+		OFMatch match = new OFMatch();
+		match.loadFromPacket(pi.getPacketData(), pi.getInPort());
+
+		int destIP = match.getNetworkDestination();
+		int sourceIP = match.getNetworkSource();
+		Short inputPort = pi.getInPort();
+		long swId = sw.getId();
+
+		// If sourceIP is discovered for the first time, store in map
+		if (!ipToSwitcId.containsKey(sourceIP)) {
+			ipToSwitcId.put(sourceIP, sw.getId());
+		}
+
+		if (destIP != 0) {
+			if (!switchToHostInfo.containsKey(swId)) {
+				Map<Integer, HostInfo> map = new HashMap<>();
+				switchToHostInfo.put(swId, map);
+			}
+			if (!switchToHostInfo.get(swId).containsKey(sourceIP)) {
+				HostInfo info = new HostInfo();
+				info.sourceIp = sourceIP;
+				info.port = inputPort;
+				switchToHostInfo.get(swId).put(sourceIP, info);
+			}
+
+			// If dest IP is already discovered then install the forward and
+			// reverse rules
+			if (switchToHostInfo.get(swId).containsKey(destIP)
+					&& (match.getDataLayerType() == Ethernet.TYPE_ARP || match
+							.getDataLayerType() == Ethernet.TYPE_IPv4
+							&& match.getNetworkProtocol() == IPv4.PROTOCOL_ICMP)) {
+				installRule(sw, match);
+
+				OFMatch reverseMatch = match
+						.clone()
+						.setDataLayerSource(match.getDataLayerDestination())
+						.setDataLayerDestination(match.getDataLayerSource())
+						.setNetworkSource(match.getNetworkDestination())
+						.setNetworkDestination(match.getNetworkSource())
+						.setInputPort(
+								switchToHostInfo.get(sw.getId()).get(
+										match.getNetworkDestination()).port);
+				installRule(sw, reverseMatch);
+			}
+
+		}
+		// Flood the packet
+		System.out.println("flooding-----");
+		this.pushPacket(sw, match, pi, (short) OFPort.OFPP_FLOOD.getValue());
+		return Command.CONTINUE;
+	}
+
+	private void installRule(IOFSwitch sw, OFMatch match) {
+		short outPort = switchToHostInfo.get(sw.getId()).get(
+				match.getNetworkDestination()).port;
+		System.out.println("Output port:" + outPort);
+
+		// create the rule
+		OFFlowMod rule = (OFFlowMod) floodlightProvider.getOFMessageFactory()
+				.getMessage(OFType.FLOW_MOD);
+		setBasicPropForRule(rule);
+		// set the Flow Removed bit
+		rule.setFlags(OFFlowMod.OFPFF_SEND_FLOW_REM);
+
+		// set of actions to apply to this rule
+		ArrayList<OFAction> actions = new ArrayList<OFAction>();
+		OFAction outputTo = new OFActionOutput(outPort);
+		actions.add(outputTo);
+		// If packet of type ICMP
+		if (match.getDataLayerType() == Ethernet.TYPE_IPv4) {
+			match.setWildcards(Wildcards.FULL.matchOn(Flag.DL_TYPE)
+					.matchOn(Flag.IN_PORT).matchOn(Flag.NW_PROTO)
+					.withNwSrcMask(32).withNwDstMask(32));
+		}
+		// If packet of type ARP
+		else {
+			match.setWildcards(Wildcards.FULL.matchOn(Flag.DL_TYPE)
+					.matchOn(Flag.IN_PORT).withNwSrcMask(32).withNwDstMask(32));
+		}
+		sendFlowMod(sw, rule, actions, match);
+	}
+
+	private void sendFlowMod(IOFSwitch sw, OFFlowMod rule,
+			ArrayList<OFAction> actions, OFMatch match) {
+		rule.setMatch(match);
+		rule.setActions(actions);
+
+		try {
+			sw.write(rule, null);
+			logger.info("Rule installation successfull From:"
+					+ match.getNetworkSource() + " to "
+					+ match.getNetworkDestination() + " on sw:" + sw.getId());
+		} catch (Exception e) {
+			logger.error("Rule installation failed");
+			e.printStackTrace();
+		}
+	}
+
+	private void setBasicPropForRule(OFFlowMod rule) {
+		rule.setCommand(OFFlowMod.OFPFC_ADD);
+		// specify timers for the life of the rule
+		rule.setIdleTimeout(TraceRoute.FLOWMOD_DEFAULT_IDLE_TIMEOUT);
+		rule.setHardTimeout(TraceRoute.FLOWMOD_DEFAULT_HARD_TIMEOUT);
+		rule.setBufferId(OFPacketOut.BUFFER_ID_NONE);
+		// specify the length of the flow structure created
+		rule.setLength((short) (OFFlowMod.MINIMUM_LENGTH + OFActionOutput.MINIMUM_LENGTH));
+	}
+
+	private void pushPacket(IOFSwitch sw, OFMatch match, OFPacketIn pi,
+			short outport) {
+
+		// create an OFPacketOut for the pushed packet
+		OFPacketOut po = (OFPacketOut) floodlightProvider.getOFMessageFactory()
+				.getMessage(OFType.PACKET_OUT);
+
+		// update the inputPort and bufferID
+		po.setInPort(pi.getInPort());
+		po.setBufferId(pi.getBufferId());
+
+		// define the actions to apply for this packet
+		OFActionOutput action = new OFActionOutput();
+		action.setPort(outport);
+		po.setActions(Collections.singletonList((OFAction) action));
+		po.setActionsLength((short) OFActionOutput.MINIMUM_LENGTH);
+
+		// set data if it is included in the packet in but buffer id is NONE
+		if (pi.getBufferId() == OFPacketOut.BUFFER_ID_NONE) {
+			byte[] packetData = pi.getPacketData();
+			po.setLength(U16.t(OFPacketOut.MINIMUM_LENGTH
+					+ po.getActionsLength() + packetData.length));
+			po.setPacketData(packetData);
+		} else {
+			po.setLength(U16.t(OFPacketOut.MINIMUM_LENGTH
+					+ po.getActionsLength()));
+		}
+
+		// push the packet to the switch
+		try {
+			sw.write(po, null);
+		} catch (IOException e) {
+			logger.error("failed to write packetOut: ", e);
+		}
+	}
+
+}
